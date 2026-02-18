20d79c8ce145d3e5c01c67faec3acaffc53255f8
Canh Nguyen Van <canhnvuit@gmail.com>
2026-02-18 08:45:37 +0700

    [Happy Coding] Init the Bookings Manager App

---
+++ bookings-manager-app/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <p>Hello React JS</p>
+      <p>
+        I am learning React JS. I promise to complete the React Hooks in Action
+        book.
+      </p>
+      <p>Let's build something amazing together!</p>
+      <p>THANKS and HAPPY CODDING</p>
+    </div>
+  );
+};
+
+export default App;
