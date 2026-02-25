866ed94f99fee8ade7a5fe72463cec45d01821e1
Canh Nguyen Van <canhnvuit@gmail.com>
2026-02-25 15:05:55 +0700

    Calling useState multiple times to work with multiple values: Using a dropdown list to set a state

--- bookings-manager-app/src/components/Bookables/BookablesList.tsx
+++ bookings-manager-app/src/components/Bookables/BookablesList.tsx
@@ -1,23 +1,48 @@
 import { useState } from "react";
 import { bookables } from "../../static.json";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function BookablesList() {
-  const group = "Rooms";
+  const [group, setGroup] = useState("Kit");
+  const [bookableIndex, setBookableIndex] = useState(0);
+  
+  const groups = [...new Set(bookables.map((b) => b.group))];
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
+
+  function nextBookable() {
+    // pass a function to setBookableIndex to get the latest state value: (i) => (i + 1) % bookablesInGroup.length
+    // use previous state value to calculate the next index, and wrap around using modulo operator
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+  }
 
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((bookable, index) => (
-        <li
-          key={bookable.id}
-          className={index === bookableIndex ? "selected" : undefined}
-        >
-          <button className="btn" onClick={() => setBookableIndex(index)}>
-            {bookable.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <select value={group} onChange={(e) => setGroup(e.target.value)}>
+        {groups.map((g) => (
+          <option key={g} value={g}>
+            {g}
+          </option>
+        ))}
+      </select>
+
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((bookable, index) => (
+          <li
+            key={bookable.id}
+            className={index === bookableIndex ? "selected" : undefined}
+          >
+            <button className="btn" onClick={() => setBookableIndex(index)}>
+              {bookable.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button className="btn" autoFocus onClick={nextBookable}>
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 }
