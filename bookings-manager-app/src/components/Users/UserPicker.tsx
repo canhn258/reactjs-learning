29df580b5f014a7ffbd0e196c7a53778d0e3c613
Canh Nguyen Van <canhnvuit@gmail.com>
2026-02-28 20:43:43 +0700

    Showing details for the selected user

--- bookings-manager-app/src/components/Users/UserPicker.tsx
+++ bookings-manager-app/src/components/Users/UserPicker.tsx
@@ -2,12 +2,9 @@ import { users } from "../../static.json";
 
 export default function UserPicker() {
   return (
-    <select name="user" id="user">
-      <option value="">Users</option>
+    <select>
       {users.map((user) => (
-        <option key={user.id} value={user.id}>
-          {user.name}
-        </option>
+        <option key={user.id}>{user.name}</option>
       ))}
     </select>
   );
--- bookings-manager-app/src/components/Users/UsersList.tsx
+++ bookings-manager-app/src/components/Users/UsersList.tsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { users } from "../../static.json";
 
 export default function UsersList() {
   const [userIndex, setUserIndex] = useState(0);
+  const user = users[userIndex];
 
   return (
-    <ul className="users items-list-nav">
-      {users.map((user, index) => (
-        <li
-          key={user.id}
-          className={index === userIndex ? "selected" : undefined}
-        >
-          <button className="btn" onClick={() => setUserIndex(index)}>
-            {user.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <ul className="users items-list-nav">
+        {users.map((user, index) => (
+          <li
+            key={user.id}
+            className={index === userIndex ? "selected" : undefined}
+          >
+            <button className="btn" onClick={() => setUserIndex(index)}>
+              {user.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="user-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
